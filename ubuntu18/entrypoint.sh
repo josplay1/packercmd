272aae50b3085699d22d9bd94acb16f88a0981aa
josplay1 <josplay.net@gmail.com>
2025-04-15 21:02:18 +0700

    Create entrypoint.sh

---
+++ ubuntu18/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sleep 2
+
+cd /home/container
+
+if [ "${GAME_AUTOUPDATE}" == "1" ]; then
+    ./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +login anonymous +force_install_dir /home/container +app_update 1110390 +quit
+fi
+
+if [ "${ROCKET_AUTOUPDATE}" == "1" ]; then
+    cp -r Extras/Rocket.Unturned Modules/
+fi
+
+mkdir -p .steam/sdk64
+cp -f steam/linux64/steamclient.so .steam/sdk64/steamclient.so
+
+mkdir -p Unturned_Headless_Data/Plugins/x86_64
+cp -f steam/linux64/steamclient.so Unturned_Headless_Data/Plugins/x86_64/steamclient.so
+
+ulimit -n 2048
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Unturned_Headless_Data/Plugins/x86_64/
+
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+${MODIFIED_STARTUP}
